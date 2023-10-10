15ac9b662a16999291fedc5e85b99618c7dec253
Leonardo Decanini <leodede04@gmail.com>
2023-10-09 21:35:07 -0300

    asd

--- resources/views/search-results.blade.php
+++ resources/views/search-results.blade.php
@@ -4,45 +4,49 @@
     <div class="marginTop section section-properties">
         <div class="container">
             <div class="row">
-                @foreach ($properties as $property)
-                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
-                        <div class="property-item mb-30">
-
-                            <a href="property-single.html" class="img">
-                                <img src="{{ $property->image_path }}" alt="Image" class="img-fluid">
-                            </a>
-
-                            <div class="property-content">
-                                <div class="price mb-2"><span>${{ $property->price }}</span></div>
-                                <div>
-                                    <span class="d-block mb-2 text-black-50"> {{ $property->address }}</span>
-                                    <span class="city d-block mb-3">{{ $property->city }}, {{ $property->country }}</span>
-
-                                    <div class="specs d-flex mb-4">
-                                <span class="d-block d-flex align-items-center me-3">
-                                    <span class="icon-bed me-2"></span>
-                                    <span class="caption">{{ $property->bedroom }}</span>
-                                </span>
-                                <span class="d-block d-flex align-items-center me-3">
-                                    <span class="icon-bath me-2"></span>
-                                    <span class="caption">{{ $property->bathroom }}</span>
-                                </span>
-                                <span class="d-block d-flex align-items-center me-3">
-                                    <span class="fa-solid fa-warehouse me-2"></span>
-                                    <span class="caption">{{ $property->garage }}</span>
-                                </span>
-                                <span class="d-block d-flex align-items-center">
-                                    <span class="fa-solid fa-stairs me-2"></span>
-                                    <span class="caption">{{ $property->floors }}</span>
-                                </span>
-                            </div>
-
-                                    <a href="property-single.html" class="btn btn-primary py-2 px-3">See details</a>
+                @if(count($properties) > 0)
+                    @foreach ($properties as $property)
+                        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
+                            <div class="property-item mb-30">
+                                <a href="property-single.html" class="img">
+                                    <img src="{{ $property->image_path }}" alt="Image" class="img-fluid">
+                                </a>
+                                <div class="property-content">
+                                    <div class="price mb-2"><span>${{ $property->price }}</span></div>
+                                    <div>
+                                        <span class="d-block mb-2 text-black-50">{{ $property->address }}</span>
+                                        <span class="city d-block mb-3">{{ $property->city }}, {{ $property->country }}</span>
+                                        <div class="specs d-flex mb-4">
+                                            <span class="d-block d-flex align-items-center me-3">
+                                                <span class="icon-bed me-2"></span>
+                                                <span class="caption">{{ $property->bedroom }}</span>
+                                            </span>
+                                            <span class="d-block d-flex align-items-center me-3">
+                                                <span class="icon-bath me-2"></span>
+                                                <span class="caption">{{ $property->bathroom }}</span>
+                                            </span>
+                                            <span class="d-block d-flex align-items-center me-3">
+                                                <span class="fa-solid fa-warehouse me-2"></span>
+                                                <span class="caption">{{ $property->garage }}</span>
+                                            </span>
+                                            <span class="d-block d-flex align-items-center">
+                                                <span class="fa-solid fa-stairs me-2"></span>
+                                                <span class="caption">{{ $property->floors }}</span>
+                                            </span>
+                                        </div>
+                                        <a href="property-single.html" class="btn btn-primary py-2 px-3">See details</a>
+                                    </div>
                                 </div>
-                            </div>
-                        </div> <!-- .item -->
+                            </div> <!-- .item -->
+                        </div>
+                    @endforeach
+                @else
+                    <div class="col-12">
+                        <div class="alert alert-warning" role="alert">
+                            Properties not found.
+                        </div>
                     </div>
-                @endforeach
+                @endif
             </div>
         </div>
     </div>
