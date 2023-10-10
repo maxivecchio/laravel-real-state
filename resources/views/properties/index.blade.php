25f919c8c35378130b40c37f82031d0119322e44
Maxi Vecchio <maximilianovecchio@gmail.com>
2023-10-09 21:47:39 -0300

    asd

--- resources/views/properties/index.blade.php
+++ resources/views/properties/index.blade.php
@@ -26,31 +26,36 @@
             <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
                 <div class="property-item mb-30">
 
-                    <a href="property-single.html" class="img">
+                    <a href="/properties/{{$property->id}}" class="img">
                         <img src="{{ $property->image_path}}" alt="Image" class="img-fluid">
                     </a>
 
                     <div class="property-content">
-                        <div class="price mb-2"><span>${{ $property->price }}</span></div>
+                        <div class="price mb-2"><span>${{ number_format($property->price) }}</span> </div>
                         <div>
                             <span class="d-block mb-2 text-black-50"> {{ $property->address }}</span>
+                            <span>sq ft: {{ number_format($property->size) }}</span>
                             <span class="city d-block mb-3">{{ $property->city }}, {{ $property->country }}</span>
 
-                            <div class="specs d-flex mb-4">
-                                <span class="d-block d-flex align-items-center me-3">
+                            <div class="specs d-flex mb-4 flex-wrap gap-3">
+                                <span class="d-flex align-items-center">
                                     <span class="icon-bed me-2"></span>
                                     <span class="caption">{{ $property->bedroom }}</span>
                                 </span>
-                                <span class="d-block d-flex align-items-center me-3">
+                                <span class="d-flex align-items-center">
                                     <span class="icon-bath me-2"></span>
                                     <span class="caption">{{ $property->bathroom }}</span>
                                 </span>
-                                <span class="d-block d-flex align-items-center me-3">
+                                <span class="d-flex align-items-center">
                                     <span class="fa-solid fa-warehouse me-2"></span>
                                     <span class="caption">{{ $property->garage }}</span>
                                 </span>
-                                <span class="d-block d-flex align-items-center">
+                                <span class="d-flex align-items-center">
                                     <span class="fa-solid fa-stairs me-2"></span>
+                                    <span class="caption">{{ $property->floors }}</span>
+                                </span>
+                                <span class="d-flex align-items-center">
+                                    <span class="fa-solid fa-kitchen-set me-2"></span>
                                     <span class="caption">{{ $property->floors }}</span>
                                 </span>
                             </div>
